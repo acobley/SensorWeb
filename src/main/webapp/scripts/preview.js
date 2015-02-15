ca6e3b34feeb990d638df6e83f6eaa4073336bef
Andy C <acobley@computing.dundee.ac.uk>
2015-02-15 17:39:03 +0000

    jquery for sensor view (incomplete)

--- src/main/webapp/scripts/preview.js
+++ src/main/webapp/scripts/preview.js
@@ -27,12 +27,36 @@ function OnMouseIn(elem) {
     $("#preview").append("<h2>Preview</h2>");
     $.getJSON(selem, function (data)
     {
+        $("#preview").append("<h3>Meta Data</h3>");
         var meta = data["Meta"];
         for (i in meta) {
 
             k = meta[i];
             $("#preview").append(i + " : " + k + "<br>");
 
+        }
+         $("#preview").append("<h3>Available Dates</h3>");
+        var dates=data["Dates"];
+        for (i in dates){
+            k=dates[i];
+            $("#preview").append( k + "<br>");
+        }
+        
+        var sensors=data["SensorList"];
+        if (sensors!=null){
+            for (i in sensors){
+                for (k in sensors[i]){
+                    j=sensors[i][k];
+                    for (l in j){
+                        t=j[l];
+                    }
+                    $("#preview").append(k + " : " + j + "<br>");
+                }
+               
+            
+
+        
+            }
         }
     });
 
