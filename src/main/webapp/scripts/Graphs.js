62bafc976937108578bab7c90e1280a3c2b74b5f
Andy C <acobley@computing.dundee.ac.uk>
2015-03-09 17:53:01 +0000

    Scaling Drawings

--- src/main/webapp/scripts/Graphs.js
+++ src/main/webapp/scripts/Graphs.js
@@ -8,47 +8,56 @@
 function drawGraph(Data) {
     Width = 300;
     Height = 200;
-    if ((typeof svg == 'undefined')) {
-        svg = d3.select("body").append("svg").attr("width", Width).attr(
-                "Height", Height);
-    }
-    var ymin=d3.min(Data, function(d){
-        return d.value;});
-    var ymax=d3.max(Data, function(d){
-        return d.value;});
-    var yscale= d3.scale.linear()
-            .domain ([d3.min(Data, function(d){return d.value;})],[d3.max(Data, function(d){return d.value;})])
-            .range([0,Height]);
-    var xscale=d3.scale.linear()
-            .domain ([0,Data.length()])
-            .range([0,Width]);
-    var circles = svg.selectAll("circle").data(Data).enter()
-			.append("circle");
+    var Datalength = Data.length;
+    svg = d3.select("body").append("svg").attr("width", Width).attr("Height", Height);
+    var ymin = d3.min(Data, function (d) {
+        return parseInt(d.value, 10);
+    });
+    var ymax = d3.max(Data, function (d) {
+        return parseInt(d.value, 10);
+    });
 
-	circles.attr("cx", function(d,i) {
-                
-		return xscale(i);
-	}).attr("cy", function(d,i) {
-		return yscale(d.value);
-	}).attr("r", function(d,i) {
-		return 2;
-	});
+    var yscale = d3.scale.linear()
+            .domain([d3.min(Data, function (d) {
+                    return parseInt(d.value, 10);
+                })], [d3.max(Data, function (d) {
+                    return parseInt(d.value, 10);
+                })])
+            .range([0, Height]);
+    var xscale = d3.scale.linear()
+            .domain([0, Datalength])
+            .range([0, Width]);
+    var circles = svg.selectAll("circle").data(Data).enter()
+            .append("circle");
+
+    circles.attr("cx", function (d, i) {
+        return xscale(i);
+    }).attr("cy", function (d, i) {
+        return yscale(parseInt(d.value, 10));
+    }).attr("r", function (d, i) {
+        return 2;
+    });
 }
 
-function getGraphsData(){
-    d3.json(path+"/JSON", function(error, data){
-        if (error){
+function getGraphsData() {
+    d3.json(path + "/JSON", function (error, data) {
+        if (error) {
             console.log(error);
-        } else{
-            var readings=data["D3Readings"];
-            var keys=readings.Sensor0;
-            var temperature=readings["Sensor0"];
-            
-            drawGraph(temperature);
+        } else {
+            var readings = data["D3Readings"];
+
+
+
+            for (var i in readings) {
+                var reading = readings[i];
+                drawGraph(reading);
+                console.log(i);
+            }
+
         }
     });
 
 }
 
 
-window.onload= getGraphsData;
+window.onload = getGraphsData;
